e5c30c698a11ed91afcf97bee16f7a0314840423
Filipe <xuting1108@hotmail.com>
2020-04-27 19:06:22 -0300

    Exercise one js done

---
+++ js/js_level_one/js_part_three/exercise.js
@@ -0,0 +1,5 @@
+alert("Hello! Convert Here :)")
+var number = prompt("Insert your value in pounds(lbs) to convert to kilograms(Kg)") 
+var convert = number * 0.454
+alert("Your value is: "+convert+"Kg")
+console.log("Conversion Completed")
