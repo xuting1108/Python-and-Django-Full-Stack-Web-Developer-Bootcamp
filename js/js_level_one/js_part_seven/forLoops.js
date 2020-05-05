9af0848fee7cab0ee119d15d600e08bdfa6ee7f9
Filipe <xuting1108@hotmail.com>
2020-05-05 18:24:09 -0300

    Add loop files

---
+++ js/js_level_one/js_part_seven/forLoops.js
@@ -0,0 +1,11 @@
+for (let index = 0; index < 5; index++) {
+    console.log("number is " +index)
+    
+}
+
+let word = "ABCDEFG"
+for (let index = 0; index < word.length; index++) {
+    console.log(word[index]);
+    
+    
+}
